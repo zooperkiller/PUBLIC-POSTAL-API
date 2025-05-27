4e7c6850994de9730328d4c3a46ef6bb6bfdc54d
Anish Thakurta <anishthakurta@gmail.com>
2025-05-27 22:53:40 +0530

    apex-lwc

--- force-app/main/default/lwc/lwr_PostalApiDetails/lwr_PostalApiDetails.js
+++ force-app/main/default/lwc/lwr_PostalApiDetails/lwr_PostalApiDetails.js
@@ -1,3 +1,29 @@
 import { LightningElement } from 'lwc';
+import getPincodeDetails from  '@salesforce/apex/REST_PostalApiController.getDetailsByPincode';
+export default class Lwr_PostalApiDetails extends LightningElement {
 
-export default class Lwr_PostalApiDetails extends LightningElement {}
+
+    pincode = '';
+  isValid = false;
+
+  handleInputChange(event) {
+    this.pincode = event.target.value;
+
+    // Validation: exactly 6 digits, all numeric
+    const regex = /^\d{6}$/;
+    this.isValid = regex.test(this.pincode);
+  }
+
+  handleSubmit() {
+    // Your logic here, e.g. calling Apex
+    console.log('Submitted Pincode:', this.pincode);
+    getPincodeDetails({pinCode: this.pincode})
+    .then(result=>{
+      console.log('@@result',result);
+      
+    })
+    .catch(error=>{
+      console.log('@@error',error);
+    })
+  }
+}
